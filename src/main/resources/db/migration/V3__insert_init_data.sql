d299b5c8502e98f79fc9c7e045253ff8558cb7c8
NervousOrange <zch693922442@outlook.com>
2020-03-02 19:12:17 +0100

    对项目配置集成测试，编写集成测试

--- src/main/resources/db/migration/V3__insert_init_data.sql
+++
@@ -1,5 +0,0 @@
-/*insert into user (username,encrypted_password,created_at,updated_at)
-values ("chenghao", "encodedPassword", now(), now())
-
-insert into blog (title, description, content, user_id, created_at, updated_at)
-values ("titleaaa", "descriptionbbb", "contentccccc", 1, now(), now())*/
